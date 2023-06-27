7a57127be00ec24f3fa3ab79c011f1582a60c1dd
Pridon Tetradze <pridontetradze@gmail.com>
2023-06-27 21:30:37 +0400

    add confirm password and validation messages

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  NonNullableFormBuilder,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,22 +16,36 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class RegisterComponent {
   registerForm = this.fb.group({
     name: [
-      'Oscar Peterson',
+      '',
       [
         Validators.required,
         Validators.pattern(/^[A-z]/),
         Validators.minLength(2),
         Validators.maxLength(20),
+        this.badNameValidator('badname'),
       ],
     ],
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: [''],
     positions: this.fb.array([this.fb.control('')]),
   });
 
   // გამოიყენეთ NonNullableFormBuilder რათა reset()-მა
   // კონტროლები გადაიყვანოს საწყის მნიშვნელობაზე და არა null-ზე.
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: NonNullableFormBuilder) {
+    this.controls.confirmPassword.setValidators([
+      this.confirmPasswordValidator(this.controls.password),
+      Validators.required,
+    ]);
+    this.controls.name.valueChanges.subscribe(() => {
+      console.log(this.controls.name);
+    });
+  }
+
+  get controls() {
+    return this.registerForm.controls;
+  }
 
   get positions() {
     return this.registerForm.controls['positions'];
@@ -45,5 +66,22 @@ export class RegisterComponent {
 
   addPosition() {
     this.positions.push(this.fb.control(''));
+  }
+
+  badNameValidator(pattern: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return control.value.includes(pattern)
+        ? { badName: 'Includes bad name!' }
+        : null;
+    };
+  }
+
+  confirmPasswordValidator(compareTo: AbstractControl): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (control.value !== compareTo.value) {
+        return { confirmPassword: 'Passwords do not match' };
+      }
+      return null;
+    };
   }
 }
